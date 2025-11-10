7ec2eae93b5f1b7c3da34fc256bfb9b2cdeb4ded
Victoria Vrublevskaya <vivrubl16@gmail.com>
2025-11-11 00:29:45 +0400

    feat: implement dns-stats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  const stats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+
+    let currentKey = '';
+
+    for (const part of parts) {
+
+      currentKey = currentKey + '.' + part;
+      stats[currentKey] = (stats[currentKey] || 0) + 1;
+    }
+  }
+
+  return stats;
 }
 
 module.exports = {
